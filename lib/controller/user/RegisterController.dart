7ab18a9591096ab1e2f3949521fd16ed3133efef
Cooltechw <wenlong.hu91@gmail.com>
2022-11-10 15:59:35 +0100

    op

--- lib/controller/user/RegisterController.dart
+++ lib/controller/user/RegisterController.dart
@@ -1,5 +1,7 @@
 import 'package:get/get.dart';
 import 'package:flutter_login/flutter_login.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
 
 class RegisterController extends GetxController {
   Duration get loginTime => Duration(milliseconds: 2250);
--- lib/views/HomePage.dart
+++ lib/views/HomePage.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    print(dotenv.env['API_URL']);
     return Container(
       child: Center(child: Text('Home Page')),
     );
