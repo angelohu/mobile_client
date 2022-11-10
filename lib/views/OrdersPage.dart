1aff8b8d41e5eaaefe55436a6a512c16201761dd
angelohu <wenlong.hu91@gmail.com>
2022-11-10 16:29:55 +0100

    ok

--- lib/views/OrdersPage.dart
+++ lib/views/OrdersPage.dart
@@ -5,6 +5,7 @@ class OrdersPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    print('object');
     return Container(
       child: Center(child: Text('Orders Page')),
     );
